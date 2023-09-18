ce7dd5e5916b417d515d83a242fa899bfcd67cb4
SayeedMahdi <sayeedmahdimousavi789@gmail.com>
2023-09-18 09:16:35 +0430

    work is done

--- src/app/room/[roomId]/page.jsx
+++ src/app/room/[roomId]/page.jsx
@@ -1,15 +1,17 @@
 "use client"
-import React, { useState, useRef } from "react"
+import React, { useState, useRef, useEffect, use } from "react"
 import  {io}  from "socket.io-client"
 const mediasoupClient = require("mediasoup-client")
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 
 
 
 
 export default function Page (){
   const urlParams = useParams();
-  const [stream, setStream] = useState(null)
+  let userStream;
+  const router = useRouter();
+
 
   //  const url = window.location.href
    const roomName = urlParams.roomId;
@@ -21,11 +23,13 @@ export default function Page (){
     let consumerTransports = []
     let audioProducer
     let videoProducer
+    let videoTrack
+    let audioTrack
   const videoRef = useRef(null);
 
 
   
-  const socket = io("http://127.0.0.1:4000/mediasoup",{
+  const socket = io("http://45.33.96.47:4000/mediasoup",{
     transports: ["websocket", "polling"],
   })
    
@@ -88,10 +92,9 @@ export default function Page (){
   
   const getLocalStream = async() => {
     try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-      setStream(stream)
-      videoRef.current.srcObject = stream;
-      streamSuccess(stream)
+      userStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+      videoRef.current.srcObject = userStream;
+      streamSuccess(userStream)
       // audioRef.current.srcObject = stream
     } catch (error) {
       console.error(error);
@@ -358,8 +361,40 @@ socket.on('producer-closed', ({ remoteProducerId }) => {
   videoContainer.removeChild(document.getElementById(`td-${remoteProducerId}`))
 })
 
+const toggleVideo = (event) => {
+   videoTrack = userStream.getTracks().find(track => track.kind === 'video');
+    if (videoTrack.enabled) {
+        videoTrack.enabled = false;
+        event.target.style.backgroundColor = "rgb(226 232 240 / 1)";
+        event.target.style.color = "black";
+        event.target.style.borderRadius = "9999px";
+       
+    } else {
+        videoTrack.enabled = true;
+        event.target.style.backgroundColor = "";
+        event.target.style.color = "white";
+    }
+}
 
+const toggleAudio = (event) => {
+   audioTrack = userStream.getTracks().find(track => track.kind === 'audio');
+    if (audioTrack.enabled) {
+        audioTrack.enabled = false;
+        event.target.style.backgroundColor = "rgb(226 232 240 / 1)";
+        event.target.style.color = "black";
+        event.target.style.borderRadius = "9999px";
+    } else {
+        audioTrack.enabled = true;
+        event.target.style.backgroundColor = "";
+        event.target.style.color = "white";
+    }
+}
 
+//leave room
+const disconnect = () => {
+  socket.on('disconnect')
+  router.push('/')
+}
 
 
   return (
@@ -371,7 +406,8 @@ socket.on('producer-closed', ({ remoteProducerId }) => {
           <tr className="align-top">
             <td>
               <div className="p-2 rounded-lg bg-slate-200 flex justify-center">
-                <video id="localVideo" ref={videoRef} autoPlay  className="w-96 bg-black mx-2" ></video>
+                <audio id="localVideo" ref={videoRef} autoPlay  className="w-96 bg-black mx-2" ></audio>
+                <video id="localVideo" ref={videoRef} autoPlay  className="w-96 bg-black mx-2" ></video> 
               </div>
             </td>
             <td>
@@ -386,17 +422,12 @@ socket.on('producer-closed', ({ remoteProducerId }) => {
         </table>
         <div className="flex flex-row relative  items-center justify-center">
           <div className="absolute -bottom-64 space-x-2 m-auto">
-      <button className="bg-red-600 hover:bg-red-700 rounded-full h-10 w-10">
+      <button onClick={disconnect} className="bg-red-600 hover:bg-red-700 rounded-full h-10 w-10">
         <svg xmlns="http://www.w3.org/2000/svg" className="ionicon h-6 w-6 text-white ml-2" viewBox="0 0 512 512">
         <path d="M451 374c-15.88-16-54.34-39.35-73-48.76-24.3-12.24-26.3-13.24-45.4.95-12.74 9.47-21.21 17.93-36.12 14.75s-47.31-21.11-75.68-49.39-47.34-61.62-50.53-76.48 5.41-23.23 14.79-36c13.22-18 12.22-21 .92-45.3-8.81-18.9-32.84-57-48.9-72.8C119.9 44 119.9 47 108.83 51.6A160.15 160.15 0 0083 65.37C67 76 58.12 84.83 51.91 98.1s-9 44.38 23.07 102.64 54.57 88.05 101.14 134.49S258.5 406.64 310.85 436c64.76 36.27 89.6 29.2 102.91 23s22.18-15 32.83-31a159.09 159.09 0 0013.8-25.8C465 391.17 468 391.17 451 374z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/>
         </svg>
       </button>
-      <button className="hover:bg-slate-200 hover:text-black rounded-full h-10 w-10">
-        <svg xmlns="http://www.w3.org/2000/svg" className="ionicon h-6 w-6 text-slate-300 ml-2  hover:text-black" viewBox="0 0 512 512">
-        <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M432 320v112H320M421.8 421.77L304 304M80 192V80h112M90.2 90.23L208 208M320 80h112v112M421.77 90.2L304 208M192 432H80V320M90.23 421.8L208 304"/>
-       </svg>
-      </button>
-      <button className="hover:bg-slate-200  hover:text-black rounded-full h-10 w-10">
+      <button onClick={toggleAudio} id="audio-btn" className="hover:bg-slate-200  hover:text-black rounded-full h-10 w-10">
         <svg xmlns="http://www.w3.org/2000/svg" className="ionicon h-6 w-6 text-white ml-2  hover:text-black" viewBox="0 0 512 512">
         <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M192 448h128M384 208v32c0 70.4-57.6 128-128 128h0c-70.4 0-128-57.6-128-128v-32M256 368v80"/><path d="M256 64a63.68 63.68 0 00-64 64v111c0 35.2 29 65 64 65s64-29 64-65V128c0-36-28-64-64-64z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/>
        </svg>
@@ -408,7 +439,7 @@ socket.on('producer-closed', ({ remoteProducerId }) => {
        </svg>
 
       </button>
-      <button className="hover:bg-slate-200 hover:text-black rounded-full h-10 w-10">
+      <button onClick={toggleVideo} className={`hover:bg-slate-200 hover:text-black rounded-full h-10 w-10`}>
        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="text-white w-6 h-6 ml-2  hover:text-black">
        <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
        </svg>
--- src/components/Challenge.jsx
+++ src/components/Challenge.jsx
@@ -22,10 +22,9 @@ export default function Challenge() {
     <div className='flex gap-1'>
 
   <img src="https://t3.ftcdn.net/jpg/02/43/12/34/360_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg" className="mb-4 h-23 w-2/3 rounded-tl-lg rounded-bl-lg align-middle leading-none shadow-lg" alt="Hollywood Sign on The Hill" />
-
   <img src="https://media.istockphoto.com/id/1200677760/photo/portrait-of-handsome-smiling-young-man-with-crossed-arms.jpg?b=1&s=612x612&w=0&k=20&c=t7Z7NBXf5t7jWqoFxsH7B3bgrO3_BznOOhqEXWywjOc=" className="mb-4 h-23 w-2/3 rounded-tr-lg rounded-br-lg align-middle leading-none shadow-lg" alt="Hollywood Sign on The Hill" />
     </div>
-    <div className="flex justify-center">
+    <div className="flex justify-center ">
 
 <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full" onClick={generateRoom}>
 Create Challenge
